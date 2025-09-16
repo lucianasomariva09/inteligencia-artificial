5e59f127a9d5bc677433bf009ba38d090918a5f9
lucianasomariva09 <159184331+lucianasomariva09@users.noreply.github.com>
2025-09-16 11:18:18 -0300

    16/09

--- main.js
+++ main.js
@@ -84,6 +84,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
@@ -94,9 +95,23 @@ function mostraPergunta(){
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
     const botaoAlternativas = document.createElement("button");
-    botaoAlternativas.textContent = alternativa;
+    botaoAlternativas.textContent = alternativa.texto;
+    botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
     caixaAlternativas.appendChild(botaoAlternativas);
+   }
 }
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes +"";
+    atual++;
+    mostraPergunta();
 }
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
 
 mostraPergunta();
